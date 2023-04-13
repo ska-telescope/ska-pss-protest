70e1709274add8ea2b51b5d9e2ed9899cc5a3558
Benjamin  Shaw <benjamin.shaw@manchester.ac.uk>
2023-04-13 12:50:55 +0000

    AT4-1018 - initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pss_protest_578390257',
+    version = '0.0.2',
+    description = 'SKA PSS Product Testing Framework test libraries',
+    url = 'https://gitlab.com/',
+    author = 'Benjamin Shaw',
+    author_email = 'ben@perfectsquares.net',
+    license = 'BSD 2-clause',
+    packages = find_packages("src"),
+    package_dir = {"": "src"},
+    include_package_data = True,
+    install_requires=['numpy',
+                      'requests',
+                      'pytest-mock',
+                      'pytest-html',
+                      'pytest-repeat',
+                      'pytest-bdd',
+                      'pytest'
+                      ],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
