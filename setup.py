bfd5fb3a5309dc7c5b472a61560547a59fc985fb
Benjamin  Shaw <benjamin.shaw@manchester.ac.uk>
2023-04-13 16:10:14 +0000

    AT4-1018 - increment version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pss_protest_578390257',
-    version = '0.0.2',
+    version = '0.0.4',
     description = 'SKA PSS Product Testing Framework test libraries',
     url = 'https://gitlab.com/',
     author = 'Benjamin Shaw',
     author_email = 'ben@perfectsquares.net',
     license = 'BSD 2-clause',
-    packages = find_packages("src"),
+    packages = find_packages(where="src"),
     package_dir = {"": "src"},
     include_package_data = True,
     install_requires=['numpy',
