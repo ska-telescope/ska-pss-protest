be98754072dd5b78f3af29d88132c59686bf984a
Benjamin  Shaw <benjamin.shaw@manchester.ac.uk>
2023-04-14 14:20:51 +0000

    AT4-1018 - Added pytest entrypoint script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pss_protest_578390257',
-    version = '0.0.4',
+    version = '0.0.6',
     description = 'SKA PSS Product Testing Framework test libraries',
     url = 'https://gitlab.com/',
     author = 'Benjamin Shaw',
@@ -11,6 +11,12 @@ setup(
     packages = find_packages(where="src"),
     package_dir = {"": "src"},
     include_package_data = True,
+    scripts = ['bin/protest'],
+    #entry_points = {
+    #    "console_scripts": [
+    #        "test_pss = ska_pss_protest.__main__:main"
+    #    ]
+    #},
     install_requires=['numpy',
                       'requests',
                       'pytest-mock',
