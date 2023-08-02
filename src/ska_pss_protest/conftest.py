f5d7e46d47f75e2942c48df00ad18f07c3fa9961
Benjamin  Shaw <benjamin.shaw@manchester.ac.uk>
2023-08-02 13:56:34 +0000

    AT4-1081 - removed fixture

--- src/ska_pss_protest/conftest.py
+++ src/ska_pss_protest/conftest.py
@@ -10,14 +10,3 @@ def pytest_addoption(parser):
     Add command line options for pytest
     """
     parser.addoption("--path", action="store", default=None)
-
-@pytest.fixture(scope="session")
-def get_vector():
-    """
-    Uses requester class to obtain test vector.
-    """
-    vector = VectorPull()
-    vector.from_name(
-        "SPS-MID_747e95f_0.2_0.0002_2950.0_0.0_Gaussian_50.0_123123123.fil"
-    )
-    yield vector
