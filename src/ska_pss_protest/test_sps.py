1a168ba2fb1d4cd5fedc787e375cbca106eb727c
benjaminshaw <benjamin.shaw@manchester.ac.uk>
2023-08-07 15:44:11 +0100

    AT4-1081 - Config generator steps added for SPS test

--- src/ska_pss_protest/test_sps.py
+++ src/ska_pss_protest/test_sps.py
@@ -43,7 +43,7 @@ def config():
     can be edited for this specific test
     """
     template_path = os.path.join(
-        DATA_DIR, "config_templates/ingest_export.xml"
+        DATA_DIR, "config_templates/mid_single_beam.xml"
     )
     assert os.path.isfile(template_path)
     tree = et.parse(template_path)
@@ -63,7 +63,7 @@ def pull_test_vector(context, vector_type, freq, dm, width, sn):
     """
     Get test vector and add path to it to the config file
     """
-    test_vector = VectorPull(cache_dir="/skatvnas3/cache")
+    test_vector = VectorPull(cache_dir="/home/benshaw/.cache/SKA/test_vectors/temp")
     test_vector.from_properties(vectype=vector_type, freq=freq, duty=width, disp=dm, sig=sn)
 
     vector_header = VHeader(test_vector.local_path)
@@ -72,27 +72,40 @@ def pull_test_vector(context, vector_type, freq, dm, width, sn):
     context["test_vector"] = test_vector 
     context["vector_header"] = vector_header
 
+    # Verify that the test vector downloaded
+    assert os.path.isfile(test_vector.local_path)
+
+
 @given("A cheetah configuration to ingest the test vector")
 def set_source(context, config):
     """
     Set test vector source in cheetah config
     """
-    pass
-
+    config("beams/beam/source/sigproc/file", context["test_vector"].local_path)
+    config_path = os.path.join("/tmp", next(tempfile._get_candidate_names()))
+    context["config_path"] = config_path
 
 @given("A cheetah configuration to export SPS filterbanked candidate data and SPS candidate metadata")
 def set_sink(config, context):
-    pass
 
+    # Set output location for candidate filterbanks
+    outdir = tempfile.mkdtemp()
+    config("beams/beam/sinks/sink_configs/sigproc/dir", outdir)
 
+    # Set output location for candidate metdata files
+    config("beams/beam/sinks/sink_configs/sp_candidate_data/dir", outdir)
+    
 @when("An SPS pipeline runs")
-def run_cheetah(context, pytestconfig):
+def run_cheetah(context, config, pytestconfig):
     """
     Add SpCcl output directory to config and
     run cheetah
     """
-    pass
+    # Set DDTR Algorithm
+    config("ddtr/klotski_bruteforce/active", "true")
+    config("sps/klotski_bruteforce/active", "true").write(context["config_path"])
 
+    # Launch cheetah with our configuration
 
 @then(
     "Candidate filterbanks are exported to disk and their header properties are consistent with the test vector"
