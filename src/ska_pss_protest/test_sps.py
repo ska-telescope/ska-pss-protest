df584d4d74b2e21e9a5e6986473d011584e9979b
benjaminshaw <benjamin.shaw@manchester.ac.uk>
2023-08-08 08:10:30 +0100

    AT4-1081 - Updated test for black compliance

--- src/ska_pss_protest/test_sps.py
+++ src/ska_pss_protest/test_sps.py
@@ -1,13 +1,5 @@
 """
-This product level test verifies the ingest and export
-capability of the PSS pipeline. The test pulls a test vector
-from the PSS test vector repository and prepares a config
-file which configures cheetah to read in and export the test vector
-as a "candidate" file that has the same duration. Cheetah will
-then run an an "empty" pipeline (a pipeline which does not
-conduct a search on the input data) and the properties of the
-exported candidate are compared to the properties of the input
-test vectors.
+Module docstring placeholder
 """
 
 import os
@@ -48,7 +40,7 @@ def config():
     assert os.path.isfile(template_path)
     tree = et.parse(template_path)
 
-    def _edit(tag, value): 
+    def _edit(tag, value):
         """
         Replace contents of tag with value
         """
@@ -58,18 +50,26 @@ def config():
     return _edit
 
 
-@given(parsers.parse("A 60 second duration {vector_type} containing {freq} pulses per second, each with a dispersion measure of {dm}, a duty cycle of {width} and a combined S/N of {sn}"))
+@given(
+    parsers.parse(
+        "A 60 second duration {vector_type} containing {freq} pulses per second, each with a dispersion measure of {dm}, a duty cycle of {width} and a combined S/N of {sn}"
+    )
+)
 def pull_test_vector(context, vector_type, freq, dm, width, sn):
     """
     Get test vector and add path to it to the config file
     """
-    test_vector = VectorPull(cache_dir="/home/benshaw/.cache/SKA/test_vectors/temp")
-    test_vector.from_properties(vectype=vector_type, freq=freq, duty=width, disp=dm, sig=sn)
+    test_vector = VectorPull(
+        cache_dir="/home/benshaw/.cache/SKA/test_vectors/temp"
+    )
+    test_vector.from_properties(
+        vectype=vector_type, freq=freq, duty=width, disp=dm, sig=sn
+    )
 
     vector_header = VHeader(test_vector.local_path)
 
     # Pass parameter from vector to context
-    context["test_vector"] = test_vector 
+    context["test_vector"] = test_vector
     context["vector_header"] = vector_header
 
     # Verify that the test vector downloaded
@@ -85,7 +85,10 @@ def set_source(context, config):
     config_path = os.path.join("/tmp", next(tempfile._get_candidate_names()))
     context["config_path"] = config_path
 
-@given("A cheetah configuration to export SPS filterbanked candidate data and SPS candidate metadata")
+
+@given(
+    "A cheetah configuration to export SPS filterbanked candidate data and SPS candidate metadata"
+)
 def set_sink(config, context):
 
     # Set output location for candidate filterbanks
@@ -94,7 +97,8 @@ def set_sink(config, context):
 
     # Set output location for candidate metdata files
     config("beams/beam/sinks/sink_configs/sp_candidate_data/dir", outdir)
-    
+
+
 @when("An SPS pipeline runs")
 def run_cheetah(context, config, pytestconfig):
     """
@@ -103,9 +107,12 @@ def run_cheetah(context, config, pytestconfig):
     """
     # Set DDTR Algorithm
     config("ddtr/klotski_bruteforce/active", "true")
-    config("sps/klotski_bruteforce/active", "true").write(context["config_path"])
+    config("sps/klotski_bruteforce/active", "true").write(
+        context["config_path"]
+    )
 
     # Launch cheetah with our configuration
+
 
 @then(
     "Candidate filterbanks are exported to disk and their header properties are consistent with the test vector"
@@ -113,6 +120,9 @@ def run_cheetah(context, config, pytestconfig):
 def validate_exported_data(context):
     pass
 
-@then("A candidate metadata file is produced which contains detections of the input signals within tolerances")
+
+@then(
+    "A candidate metadata file is produced which contains detections of the input signals within tolerances"
+)
 def do_last_bit(context):
     pass
