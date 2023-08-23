AT4-1078: Fixed missing parentheses S/N calculation in DMstepTol

src/ska_pss_protest/candlist.py
@@ -816,7 +816,7 @@ class DMstepTol:
         """
 
         sn_tol = sn_int * np.sqrt(
-            wint * (period - self.weffbox) / self.weffbox * (period - wint)
+            (wint * (period - self.weffbox)) / (self.weffbox * (period - wint))
         )
 
         self.min_sn = sn_tol