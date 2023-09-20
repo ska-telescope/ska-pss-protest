deb915525ce302aa119b2df7ec16c423542e419a
benjaminshaw <benjamin.shaw@manchester.ac.uk>
2023-09-20 14:46:45 +0100

    AT4-1194 - routed cache to call to requester

--- src/ska_pss_protest/test_sps_emulator.py
+++ src/ska_pss_protest/test_sps_emulator.py
@@ -38,11 +38,11 @@ def context():
 
 
 @pytest.fixture(scope="session")
-def get_vector():
+def get_vector(pytestconfig):
     """
     Uses requester class to obtain test vector.
     """
-    vector = VectorPull()
+    vector = VectorPull(cache_die=pytestconfig.getoption("cache"))
     vector.from_name(
         "SPS-MID_747e95f_0.2_0.0002_2950.0_0.0_Gaussian_50.0_123123123.fil"
     )
