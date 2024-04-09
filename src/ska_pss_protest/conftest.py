8df5b5327bb8d7649901f2607797a70d9c855db2
benjaminshaw <benjamin.shaw@manchester.ac.uk>
2024-04-09 21:37:19 +0100

    AT4-1330 - added cleanup

--- src/ska_pss_protest/conftest.py
+++ src/ska_pss_protest/conftest.py
@@ -3,11 +3,15 @@ Fixtures defined here can be used by any test in the
 protest package without the need to import them.
 pytest will automatically discover them.
 """
+
 import logging
-import pytest
+import os
 import shutil
 import tempfile
-import os
+
+import pytest
+
+# pylint: disable=W0212
 
 logging.basicConfig(
     format="1|%(asctime)s|%(levelname)s\
@@ -15,6 +19,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%I:%M:%S",
     level=logging.INFO,
 )
+
 
 def pytest_addoption(parser):
     """
@@ -25,12 +30,14 @@ def pytest_addoption(parser):
     parser.addoption("--outdir", action="store", default="/tmp")
     parser.addoption("--keep", action="store_true")
 
+
 @pytest.fixture(scope="function")
 def teardown():
     """
     Fixture to remove the directory into which
     data products from a test set are written
     """
+
     def _data_rm(directory):
         """
         Private fixture parameteriser
@@ -39,12 +46,14 @@ def teardown():
 
     return _data_rm
 
+
 @pytest.fixture(scope="function")
 def outdir():
     """
     Fixture to set the output directory into which
     data products for a test set are written
     """
+
     def _outdir(parent):
         """
         Private fixture parameteriser
@@ -55,35 +64,40 @@ def outdir():
         return this_outdir
 
     return _outdir
-        
+
+
 @pytest.fixture(scope="function")
 def conf():
     """
     Fixture to name the Cheetah config file
     """
+
     def _confpath(parent):
         """
         Private fixture parameteriser
         """
         if os.path.isdir(parent):
-            this_confpath = os.path.join(parent, "config_" + next(tempfile._get_candidate_names()))
+            this_confpath = os.path.join(
+                parent, "config_" + next(tempfile._get_candidate_names())
+            )
             return this_confpath
         raise FileNotFoundError("Directory {} not found".format(parent))
 
     return _confpath
 
+
 @pytest.fixture(scope="session", autouse=True)
 def cleanup(pytestconfig):
     """
-    Remove parent directory of test result data. 
+    Remove parent directory of test result data.
     The teardown() fixture above removes the data
     directories containing results for individual
     test sets. In a given run of ProTest, all of those
-    data directorys are placed in a single parent directory. 
+    data directorys are placed in a single parent directory.
     This fixture removes the parent directory if the --keep
     option is not passed to ProTest.
     """
-    yield 
+    yield
     result_dir = pytestconfig.getoption("outdir")
     if not pytestconfig.getoption("keep"):
         logging.info("Removing directory {}".format(result_dir))
